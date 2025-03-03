270c23c46c9bc8b17105445c98e98ff1e9eb099b
hhgsx <jamesedwin204@gmail.com>
2025-03-03 12:22:57 +0000

    enough to run

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+
 def create_app():
 
+    db = SQLAlchemy()
+
+    from .views import views
+
     app = Flask(__name__)
+
+    app.register_blueprint(views, url_prefix='/')
 
     return app
