1d8ccdb95b26773c359f8efca5e0e01b2cc53ed9
Lakshmi Ravikumar <lakshmi.sanjanar14@gmail.com>
2025-03-03 15:52:47 +0000

    fix event route error

--- website/__init__.py
+++ website/__init__.py
@@ -6,8 +6,11 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'  # Example for SQLite
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
-    
+
     from .views import views
     app.register_blueprint(views, url_prefix='/')
 
