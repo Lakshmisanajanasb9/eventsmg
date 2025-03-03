7e3d04576e9a3a061375e977f96d3f3f5b428cd1
Lakshmi Ravikumar <lakshmi.sanjanar14@gmail.com>
2025-03-03 14:45:29 +0000

    Writing API Tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,15 @@
+import json
+from website import create_app
+
+app = create_app
+client = app.test_client()
+
 def test_example():
     assert 1 + 1 == 2  # This should always pass
 
+def get_events():
+    response = client.get("/events")
+
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert isinstance(data, list)
