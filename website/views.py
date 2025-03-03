e360939ffb1f87913339035d4d340e21cb3c832b
hhgsx <jamesedwin204@gmail.com>
2025-03-03 12:53:30 +0000

    change database

--- website/views.py
+++ website/views.py
@@ -5,3 +5,8 @@ views = Blueprint('views' , __name__)
 @views.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
+
+
+@views.route('/example', methods=['GET', 'POST'])
+def example():
+    return render_template('example.html')
