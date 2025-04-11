ac21e841fc2b1d5ef3bc458bace49eb2d55570c0
hhgsx <jamesedwin204@gmail.com>
2025-04-11 22:44:30 +0100

    auth update

--- website/forms.py
+++ website/forms.py
@@ -11,38 +11,25 @@ forms = Blueprint('forms', __name__)
 def register():
     if request.method == "POST":
 
-        #extracts data ofthe user in the form
         email = request.form.get("email")
-        username = request.form.get("username")
         password = request.form.get("password")
 
-        #compares details of the data inputted and the existing data in database
+        with db.Session(db.engine) as session:
+            user = session.query(db.User).filter_by(email=email).first()
+
         user = User.query.filter_by(email=email).first()    
         if user:
-            #validation
             flash("email already exists",category='error')
-        elif len(username) < 5 :
-            flash("Error: username must be greater than 4 characters",category='error')
-        elif len(password)  < 7 :
-            flash("Error: password must be greater than 6 characters",category='error')
-        elif len(password)  > 21 :
-            flash("Error: password must be less then 20 characters",category='error')
-        elif not any(char.isdigit() for char in password):     #checks if the password contains numbers
-            flash("Error : PAssword does not contain at least 1 number",category='error')
-        elif not any(char.isupper() for char in password):      #checks if the password contains uppercases
-            flash("Error :password does not contain at least 1 upper case letter",category='error')
-        elif not any(char.islower() for char in password):      #checks if the password contains lowercases
-            flash("Error :password does not contain at least 1 lower case letter ",category='error')
-        else:
-            new_user = User(email=email, username=username,password=generate_password_hash(password))
-            flash('Account created',category='success')
-            #will add a new row to the database using the data entered
-            db.session.add(new_user)
-            db.session.commit()
+            return redirect(url_for('register'))
+        
+        with db.Session(db.engine) as session:
+            new_user = db.User(email=email,password=generate_password_hash(password))
+            session.add(new_user)
+            session.commit()
 
-            #the user will be redirected to the home screen after signing up
-            return redirect(url_for('views.display_manga'))
-    
+        flash("Registration complete.Please login","success")
+        return redirect(url_for("login"))
+
     return render_template('register.html')
 
 
@@ -52,15 +39,14 @@ def login():
     if request.method == "POST":
         email=request.form.get('email') 
         password=request.form.get('password')
-        user = User.query.filter_by(email=email).first()
+        with db.Session(db.engine) as session:
+            user = User.query.filter_by(email=email).first()
 
         if user and check_password_hash(user.password,password):
-            login_user(user, remember=True)
-            return redirect(url_for('views.display_manga'))
+            login_user(user)
+            return redirect(url_for('home'))
         else:
-            flash('email does not exist', category='error')
-    else:
-        flash('Email does not exist', category="error")
+            flash('Invalid email or password', category='error')
 
     return render_template('login.html')
 
