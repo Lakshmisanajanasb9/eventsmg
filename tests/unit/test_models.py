bf7be72b394af3551b33e01099680e9f46c4d349
hhgsx <jamesedwin204@gmail.com>
2025-04-13 22:08:57 +0100

    test(working)

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -7,7 +7,14 @@ def test_new_user():
     Then check email,hashed password and role fields are defined correctly
     """
 
-    customer = Customer('1','John','Cena','johncena123@gmail.com','07909756744','Chicage')
+    customer = Customer(
+        customer_id=1,
+        first_name='John',
+        last_name='Cena',
+        email='johncena123@gmail.com',
+        phone='07909756744',
+        location='Chicago'
+    )
     assert customer.customer_id == '1'
     assert customer.first_name == 'John'
     assert customer.last_name == 'Cena'
