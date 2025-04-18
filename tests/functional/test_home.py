3024e0eeeb4d37fec107c40f34f0e61ed3f7f897
hhgsx <jamesedwin204@gmail.com>
2025-04-18 22:34:12 +0100

    checks

--- tests/functional/test_home.py
+++ tests/functional/test_home.py
@@ -13,5 +13,5 @@ def test_home_page():
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
         response = test_client.post('/')
-        assert response.status_code == 405
+        assert response.status_code == 200
 
